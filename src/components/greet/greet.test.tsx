413c529b6f0bfa3c825db9d5f25d3fe0b2b342d3
Valentine OO <valoladimeji@gmail.com>
2024-12-19 04:31:12 +0100

    test(greet): add failing test for Greet component and also implement Greet component to pass tests
    - setup test to expect Hello text rendering
    - implement initial test structure
    - create Greet component with Hello text
    - satisfy test requirements

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,10 +1,31 @@
+// import { render, screen } from "@testing-library/react";
+// import Greet from "./Greet";
+
 import { render, screen } from "@testing-library/react";
 import Greet from "./Greet";
 
-describe("group tests for greet components", () => {
-  it("should test if the greet component renders correctly", () => {
-    render(<Greet />);
+// describe("group tests for greet components", () => {
+//   it("should test if the greet component renders correctly", () => {
+//     render(<Greet />);
 
+//     const textElement = screen.getByText(/hello/i);
+//     expect(textElement).toBeInTheDocument();
+//   });
+// });
+
+/**
+ * Greet should render the text hello and if a name is passed into the component it should render hello followed by the name
+ */
+
+describe("a group of tests for Greet component", () => {
+  it("should render Greet component correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
+
+  it("should render Greet component with a name", () => {
+    render(<Greet name="Valentine" />);
     const textElement = screen.getByText("Hello");
     expect(textElement).toBeInTheDocument();
   });
